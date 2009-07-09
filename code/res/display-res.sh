18567d1281df949157edc2dc2c900dec8e7e14f9
Antoine Levitt <levitt@magnesium.univ-mlv.fr>
2009-07-09 14:51:31 +0200

    pas de multiplot dans gnuplot

--- code/res/display-res.sh
+++ code/res/display-res.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo 'set multiplot' > gnuplot_commands
 echo -n 'plot ' >> gnuplot_commands
 for i in $(find . -type d -maxdepth 1 -not -name .) ; do
     echo -n "\"$i/profil_v_0.5.dat\"  using 1:$1 title \"$i\" with lines," >> gnuplot_commands
