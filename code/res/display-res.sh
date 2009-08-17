7a8a7ce95346eac653ec7c3f9c290deca56e7411
Antoine Levitt <levitt@magnesium.univ-mlv.fr>
2009-08-17 14:47:24 +0200

    display-res : linespoints, autoriser le zoom, afficher uniquement base*

--- code/res/display-res.sh
+++ code/res/display-res.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo -n 'plot ' > gnuplot_commands
-for i in $(find . -type d -maxdepth 1 -not -name .) ; do
-    echo -n "\"$i/profil_v_0.5.dat\"  using 1:$1 title \"$i\" with lines," >> gnuplot_commands
+for i in $(find . -type d -maxdepth 1 -not -name . -name 'base*') ; do
+    echo -n "\"$i/profil_v_0.5.dat\"  using 1:$1 title \"$i\" with linespoints," >> gnuplot_commands
 done
 sed -i 's/,$//' gnuplot_commands
 echo "" >> gnuplot_commands
-echo "set xrange [7:9]" >> gnuplot_commands
 echo "replot" >> gnuplot_commands
-gnuplot -persist gnuplot_commands
+echo "pause -1" >> gnuplot_commands
+gnuplot gnuplot_commands
