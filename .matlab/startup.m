88f5df9421a0c7b4d240fcc1116f05688bdf6841
Antoine Levitt <antoine.levitt@gmail.com>
2010-06-24 21:41:03 +0100

    Correction

--- .matlab/startup.m
+++ .matlab/startup.m
@@ -1,6 +1,6 @@
 addpath ~/.matlab/
 addpath ~/.matlab/userfuns
 % please dock all my figures, thank you. This way, matlab stops popping annoying figures everywhere
-if(get(0, 'MonitorPositions') ~= [0 0 1 1])
+if(any(get(0, 'MonitorPositions') ~= [0 0 1 1]))
     set(0, 'DefaultFigureWindowStyle', 'docked')
 end
