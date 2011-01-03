cbc568b6bb04a049f9006f90e4ae00a791add7eb
Antoine Levitt <antoine.levitt@gmail.com>
2011-01-03 15:35:29 +0100

    add a check against pid = 0 in raise_process (for some reason, matlab messes with window managers ...)

--- bin/raise_process.sh
+++ bin/raise_process.sh
@@ -3,14 +3,17 @@
 PIDS=$(wmctrl -lp | cut -c 15- | cut -f 1 -d ' ')
 for p in $PIDS
 do
-    LINE=$(ps $p | cut -c 28- | tail -n 1)
-    # if we get a match
-    if echo $LINE | grep "$1"
+    if [ $p != 0 ]
     then
-	ID=$(wmctrl -lp | grep $p | cut -c 1-10)
-	echo raising $p
-	wmctrl -ia $ID
-	exit 0;
+	LINE=$(ps $p | cut -c 28- | tail -n 1)
+        # if we get a match
+	if echo $LINE | grep "$1"
+	then
+	    ID=$(wmctrl -lp | grep $p | cut -c 1-10)
+	    echo raising $p
+	    wmctrl -ia $ID
+	    exit 0;
+	fi
     fi
 done
 
