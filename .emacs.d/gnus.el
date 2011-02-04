2b88ccbe606e7b82c4020b1b8ae037a01fb547b8
Antoine Levitt <antoine.levitt@gmail.com>
2011-02-04 16:12:40 +0100

    refactor some of gnus.el

--- .emacs.d/gnus.el
+++ .emacs.d/gnus.el
@@ -1,6 +1,163 @@
 ;; require stuff. Needed because I define-keys on keymaps that need to be loaded
 (require 'gnus-group)
 (require 'gnus-sum)
+(require 'gnus-art)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; General setup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq gnus-inhibit-startup-message t
+      gnus-interactive-exit nil
+      gnus-use-dribble-file nil
+      ;; don't bother querying the server about unsubscribed groups
+      gnus-activate-level gnus-level-subscribed
+      ;; I don't use any other newsreader, so don't clutter ~
+      gnus-save-newsrc-file nil
+      gnus-read-newsrc-file nil
+      ;; Please prefetch stuff
+      gnus-asynchronous t
+      ;; Don't ignore newsgroups. Default ignores []
+      gnus-ignored-newsgroups ""
+      ;; Don't gcc
+      gnus-message-archive-group nil
+      ;; Be quiet
+      gnus-verbose 4
+      gnus-verbose-backends 4
+      ;; Sort things
+      gnus-thread-sort-functions '(gnus-thread-sort-by-most-recent-date)
+      gnus-article-sort-functions '((not gnus-article-sort-by-date))
+      gnus-group-sort-function '(gnus-group-sort-by-alphabet gnus-group-sort-by-level)
+      ;; Try to fill gaps in threads
+      gnus-build-sparse-threads 'some
+      gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
+      ;; Threads display
+      gnus-sum-thread-tree-root ""
+      gnus-sum-thread-tree-single-indent ""
+      gnus-sum-thread-tree-vertical "|"
+      gnus-sum-thread-tree-indent " "
+      gnus-sum-thread-tree-leaf-with-other "+-> "
+      gnus-sum-thread-tree-single-leaf "`-> "
+      gnus-sum-thread-tree-vertical "│"
+      gnus-sum-thread-tree-leaf-with-other "├─► "
+      gnus-sum-thread-tree-single-leaf "╰─► "
+      ;; See what client people use
+      gnus-visible-headers (concat gnus-visible-headers "\\|^User-Agent:\\|^X-Mailer:")
+      ;; Dates for humans
+      gnus-user-date-format-alist
+      '(((gnus-seconds-today) . "Today, %H:%M")
+	((+ 86400 (gnus-seconds-today)) . "Yesterday, %H:%M")
+	(604800 . "%A %H:%M")
+	((gnus-seconds-month) . "%A %d")
+	((gnus-seconds-year) . "%B %d")
+	(t . "%B %d '%y"))
+      ;; Cleaner format for groups and summaries
+      gnus-group-line-format "%y %(%G %)%O\n"
+      ;; the %uB invokes a function which returns the author name from BBDB
+      gnus-summary-line-format "%U%R%~(max-right 17)~(pad-right 17)&user-date; %-30,30uB %*%B%s\n"
+      
+      ;; Performance-related settings
+      ;; How large is large?
+      gnus-large-newsgroup 1000
+      ;; suggested in the man. Not sure that's useful though.
+      gc-cons-threshold 3500000
+      ;; don't silently hide messages
+      gnus-newsgroup-maximum-articles nil
+      ;; Gnus agent is buggy for imap, so don't use this for imap (J r in server buffer)
+      ;; It's a known issue, but nobody seems to care.
+      gnus-agent t
+
+      ;; Message
+      ;; Complete using dabbrev
+      message-tab-body-function (lambda () (interactive) (dabbrev-expand nil))
+      message-citation-line-function 'message-insert-formatted-citation-line
+      ;; 01/01/1970, XX
+      message-citation-line-format "%x %R, %N"
+      ;; Default to top post
+      message-cite-reply-above 'is-evil)
+
+;; compile stuff. No idea what it does, but it's supposed to be faster
+(gnus-compile)
+
+;; Unbind stuff I use for something else
+(define-key gnus-group-mode-map (kbd "M-&") nil)
+(define-key gnus-summary-mode-map (kbd "M-&") nil)
+(define-key gnus-article-mode-map (kbd "M-&") nil)
+
+;; smaller window for summary
+(gnus-add-configuration
+ '(article
+   (vertical 1.0
+	     (summary 0.2 point)
+	     (article 1.0))))
+
+;; toggle between read and unread articles. this is a bit of a hack, and should be better integrated.
+;; oh well.
+(setq gnus-group-display-unread nil)
+(defun gnus-group-toggle-unread ()
+  (interactive)
+  (toggle-variable 'gnus-group-display-unread)
+  (gnus-group-redisplay))
+(defun gnus-group-redisplay ()
+  "Redisplay group according to gnus-group-display-unread"
+  (interactive)
+  (if (not gnus-group-display-unread)
+      (gnus-group-list-groups gnus-level-subscribed nil)
+    (gnus-group-list-all-groups gnus-level-subscribed))
+  (goto-char (point-min)))
+(define-key gnus-group-mode-map (kbd "h") 'gnus-group-toggle-unread)
+
+(defun gnus-group-bury ()
+  (interactive)
+  (gnus-group-save-newsrc)
+  (gnus-unread-update-unread-count)
+  (condition-case err
+      ;;sometimes, articles stay after gnus is quitted.
+      ;;I don't know why, I don't care, just kill them all!
+      (kill-buffer "*Article*")
+    (error nil))
+  (bury-buffer))
+;; bury instead of gnus-group-exit.
+(define-key gnus-group-mode-map (kbd "q") 'gnus-group-bury)
+
+;; When I reply to a message, I want gnus to exit the corresponding
+;; summary buffer. 
+
+;; advice that's used to exit the summary buffer
+(setq gnus-advice-exit-summary
+      '(let ((sumbuf (current-buffer)))
+	 ad-do-it
+	 (let ((replybuf (current-buffer)))
+	   (with-current-buffer sumbuf
+	     (gnus-summary-exit))
+	   (switch-to-buffer replybuf))))
+
+(defmacro gnus-add-exit-summary-to-function (fun)
+  "Advices FUN so that it exits the summary buffer."
+  (let ((advice-name
+	 (make-symbol (concat (symbol-name (eval fun)) "-and-exit-summary"))))
+    ;; the doc page for ad-add-advice says it won't add the same advice twice,
+    ;; but it lies.
+    (unless (ad-get-advice-info-field (eval fun) 'around)
+      `(progn (ad-add-advice ,fun
+			     '(,advice-name
+			       nil
+			       t
+			       (lambda ()
+				 ,gnus-advice-exit-summary))
+			     'around
+			     'last)
+	      (ad-activate ,fun)))))
+
+(gnus-add-exit-summary-to-function 'gnus-summary-reply)
+(gnus-add-exit-summary-to-function 'gnus-summary-followup)
+(gnus-add-exit-summary-to-function 'gnus-summary-mail-forward)
+
+;; if gnus doesn't respond in 15s, give up
+(defadvice gnus-demon-scan-news (around gnus-demon-timeout activate)
+  "Timeout for Gnus."
+  (with-timeout
+      (15 (message "Gnus timed out."))
+    ad-do-it))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Mairix
@@ -28,16 +185,16 @@
 ;;; Mime
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Display html or not?
-(setq mm-discouraged-alternatives '("text/html" "text/richtext"))
-(setq mm-discouraged-alternatives '())
-;; Offer to save in ~
-(setq mm-default-directory "~/")
-;; Render with gnus-w3m. Also available is shr
-(setq mm-text-html-renderer 'gnus-w3m)
-;; Useful for webcomics and such
-(setq mm-inline-text-html-with-images t)
-(setq mm-attachment-override-types '("image/.*"))
-(setq gnus-blocked-images nil)
+(setq mm-discouraged-alternatives '("text/html" "text/richtext")
+      mm-discouraged-alternatives '()
+      ;; Offer to save in ~
+      mm-default-directory "~/"
+      ;; Render with gnus-w3m. Also available is shr
+      mm-text-html-renderer 'gnus-w3m
+      ;; Display images
+      mm-inline-text-html-with-images t
+      mm-attachment-override-types '("image/.*")
+      gnus-blocked-images nil)
 
 ;; Use KH to view in a real browser
 (defun my-gnus-summary-view-html-alternative ()
@@ -56,134 +213,6 @@
 ;; In summary buffer, press m to go to the article in gwene
 (define-key gnus-summary-mode-map (kbd "m")
   "\C-xo\276\C-rlink\C-m\C-m\274\C-xo")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Misc
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; I don't use any other newsreader
-(setq gnus-save-newsrc-file nil
-      gnus-read-newsrc-file nil)
-;; Please prefetch stuff
-(setq gnus-asynchronous t)
-;; default value was : "^to\\.\\|^[0-9. 	]+\\( \\|$\\)\\|^[\"][]\"[#'()]"
-;; must have been useful for some reason, around 1970.
-(setq gnus-ignored-newsgroups "")
-;; stfu, kthx
-(setq gnus-verbose 4)
-(setq gnus-verbose-backends 4)
-;; format dates in an user-friendly way
-;; (setq gnus-user-date-format-alist '((t . "%d.%m.%Y %H:%M")))
-(setq gnus-user-date-format-alist
-      '(((gnus-seconds-today) . "Today, %H:%M")
-	((+ 86400 (gnus-seconds-today)) . "Yesterday, %H:%M")
-	(604800 . "%A %H:%M") ;;that's one week
-	((gnus-seconds-month) . "%A %d")
-	((gnus-seconds-year) . "%B %d")
-	(t . "%B %d '%y"))) ;;this one is used when no other does match
-;; default : (setq gnus-group-line-format "%M%S%p%P%5y:%B%(%g%)%O\n")
-(setq gnus-group-line-format "%y %(%G %)%O\n")
-;;(setq gnus-summary-line-format "%U%R%z%(%[%d: %-20,20n%]%)%B %s\n")
-;;(setq gnus-summary-line-format "%&user-date; %-30,30n%B%s\n")
-;; the %uB invokes a function which returns the author name from BBDB
-;;(setq gnus-summary-line-format "%U%R%&user-date; %-30,30uB %*%B%s\n")
-(setq gnus-summary-line-format "%U%R%~(max-right 17)~(pad-right 17)&user-date; %-30,30uB %*%B%s\n")
-
-;; threading display
-(setq gnus-sum-thread-tree-root ""
-      gnus-sum-thread-tree-single-indent ""
-      gnus-sum-thread-tree-vertical "|"
-      gnus-sum-thread-tree-indent " "
-      gnus-sum-thread-tree-leaf-with-other "+-> "
-      gnus-sum-thread-tree-single-leaf "`-> ")
-(setq gnus-sum-thread-tree-vertical "│")
-(setq gnus-sum-thread-tree-leaf-with-other "├─► ")
-(setq gnus-sum-thread-tree-single-leaf "╰─► ")
-
-;; smaller window for summary
-(gnus-add-configuration
- '(article
-   (vertical 1.0
-	     (summary 0.2 point)
-	     (article 1.0))))
-
-(setq gnus-inhibit-startup-message t
-      gnus-interactive-exit nil
-      gnus-use-dribble-file nil
-      ;; don't bother querying the server about unsubscribed groups
-      gnus-activate-level gnus-level-subscribed)
-
-;; toggle between read and unread articles. this is a bit of a hack, and should be better integrated.
-;; oh well.
-(setq gnus-group-display-unread nil)
-(defun gnus-group-toggle-unread ()
-  (interactive)
-  (toggle-variable 'gnus-group-display-unread)
-  (gnus-group-redisplay))
-(defun gnus-group-redisplay ()
-  "Redisplay group according to gnus-group-display-unread"
-  (interactive)
-  (if (not gnus-group-display-unread)
-      (gnus-group-list-groups gnus-level-subscribed nil)
-    (gnus-group-list-all-groups gnus-level-subscribed))
-  (goto-char (point-min)))
-(define-key gnus-group-mode-map (kbd "h") 'gnus-group-toggle-unread)
-
-
-
-;;(setq gnus-thread-sort-functions '(gnus-thread-sort-by-number (not gnus-thread-sort-by-most-recent-number)))
-(setq gnus-thread-sort-functions '(gnus-thread-sort-by-most-recent-date))
-(setq gnus-article-sort-functions '((not gnus-article-sort-by-date)))
-(setq gnus-group-sort-function '(gnus-group-sort-by-alphabet gnus-group-sort-by-level))
-
-(setq gnus-build-sparse-threads 'more)
-;;(setq gnus-thread-hide-subtree t)
-;; Gnus agent is buggy for imap, so don't use this for imap (J r in server buffer)
-;; It's a known issue, but nobody seems to care.
-(setq gnus-agent t)
-;; compile stuff. No idea what it does, but it's supposed to be faster
-(gnus-compile)
-
-(setq gnus-large-newsgroup 1000)
-
-(define-key gnus-group-mode-map (kbd "M-&") nil)
-(define-key gnus-summary-mode-map (kbd "M-&") nil)
-(define-key gnus-article-mode-map (kbd "M-&") nil)
-
-(defun gnus-group-bury ()
-  (interactive)
-  (gnus-group-save-newsrc)
-  (gnus-unread-update-unread-count)
-  (condition-case err
-      (kill-buffer "*Article*") ;;sometimes, articles stay after gnus is quitted.
-					;I don't know why, I don't care, just kill them all!
-    (error nil))
-  (bury-buffer))
-;; bury instead of gnus-group-exit.
-(define-key gnus-group-mode-map (kbd "q") 'gnus-group-bury)
-
-;; cleanup
-(defadvice save-buffers-kill-emacs (before quit-gnus (&rest args) activate)
-  (let (buf)
-    (when (and (fboundp 'gnus-alive-p)
-	       (gnus-alive-p)
-	       (bufferp (setq buf (get-buffer "*Group*"))))
-      (with-current-buffer buf
-	(gnus-group-exit)))))
-
-;; fill long
-(require 'gnus-art)
-(define-key gnus-summary-mode-map "l" 'gnus-article-fill-long-lines)
-
-;; see what clients people use
-(setq gnus-visible-headers 
-      (concat gnus-visible-headers "\\|^User-Agent:\\|^X-Mailer:"))
-
-;; if gnus doesn't respond in 15s, give up
-(defadvice gnus-demon-scan-news (around gnus-demon-timeout activate)
-  "Timeout for Gnus."
-  (with-timeout
-      (15 (message "Gnus timed out."))
-    ad-do-it))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Receive mail
@@ -311,29 +340,16 @@
 (bbdb-initialize 'gnus 'message)
 (bbdb-insinuate-message)
 (bbdb-insinuate-gnus)
-(setq bbdb/gnus-summary-mark-known-posters nil)
-(setq bbdb-always-add-addresses t)
-(setq bbdb-new-nets-always-primary t)
-(setq bbdb-offer-save 1) ; save without asking
-;; add to bbdb only if I'm recipient or cc'ed. See below for bbdb-ignore-most-messages-alist
-(setq bbdb-ignore-most-messages-alist nil)
-(setq bbdb/mail-auto-create-p 'bbdb-ignore-most-messages-hook)
-(setq bbdb-use-pop-up nil
-      bbdb-complete-name-allow-cycling t)
-(setq bbdb-dwim-net-address-allow-redundancy t)
-
-
-(setq gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references)
-
-;; suggested in the man. Not sure that's useful though.
-(setq gc-cons-threshold 3500000)
-(setq gnus-newsgroup-maximum-articles nil)
-
-(setq message-tab-body-function (lambda () (interactive) (dabbrev-expand nil)))
-
-(setq message-citation-line-function 'message-insert-formatted-citation-line)
-(setq message-citation-line-format "%x %R, %N")
-(setq gnus-message-archive-group nil)
+(setq bbdb/gnus-summary-mark-known-posters nil
+      bbdb-always-add-addresses t
+      bbdb-new-nets-always-primary t
+      bbdb-offer-save 1 ; save without asking
+      ;; add to bbdb only if I'm recipient or cc'ed. See below for bbdb-ignore-most-messages-alist
+      bbdb-ignore-most-messages-alist nil
+      bbdb/mail-auto-create-p 'bbdb-ignore-most-messages-hook
+      bbdb-use-pop-up nil
+      bbdb-complete-name-allow-cycling t
+      bbdb-dwim-net-address-allow-redundancy t)
 
 ;; Personal info for password privacy
 (load "~/.emacs.d/priv_gnus.el" t)
@@ -359,36 +375,3 @@
 ;; person. This piece of code forbids it.
 (setq bbdb-ignore-some-messages-alist '(("From" . "gwene")))
 (setq bbdb-always-add-addresses 'bbdb-ignore-some-messages-hook)
-
-;; When I reply to a message, I want gnus to exit the corresponding
-;; summary buffer. 
-
-;; advice that's used to exit the summary buffer
-(setq gnus-advice-exit-summary
-      '(let ((sumbuf (current-buffer)))
-	 ad-do-it
-	 (let ((replybuf (current-buffer)))
-	   (with-current-buffer sumbuf
-	     (gnus-summary-exit))
-	   (switch-to-buffer replybuf))))
-
-(defmacro gnus-add-exit-summary-to-function (fun)
-  "Advices FUN so that it exits the summary buffer."
-  (let ((advice-name
-	 (make-symbol (concat (symbol-name (eval fun)) "-and-exit-summary"))))
-    ;; the doc page for ad-add-advice says it won't add the same advice twice,
-    ;; but it lies.
-    (unless (ad-get-advice-info-field (eval fun) 'around)
-      `(progn (ad-add-advice ,fun
-			     '(,advice-name
-			       nil
-			       t
-			       (lambda ()
-				 ,gnus-advice-exit-summary))
-			     'around
-			     'last)
-	      (ad-activate ,fun)))))
-
-(gnus-add-exit-summary-to-function 'gnus-summary-reply)
-(gnus-add-exit-summary-to-function 'gnus-summary-followup)
-(gnus-add-exit-summary-to-function 'gnus-summary-mail-forward)
