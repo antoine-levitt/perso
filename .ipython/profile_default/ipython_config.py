7d3ddd9b68f3736ae2e90a48bbaf467d3cb50442
Antoine Levitt <antoine.levitt@gmail.com>
2011-10-29 00:21:59 +0200

    ipython config

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,7 @@
+# Configuration file for ipython.
+c = get_config()
+c.TerminalInteractiveShell.prompt_in1 = '$ '
+c.TerminalInteractiveShell.prompt_out = '> '
+c.TerminalIPythonApp.gui = 'gtk'
+c.TerminalIPythonApp.pylab = 'gtk'
+c.TerminalInteractiveShell.deep_reload = True
