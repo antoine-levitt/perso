dfac6a6f6f1e07826dceba2678986d7363a80247
Antoine Levitt <antoine.levitt@gmail.com>
2011-10-30 15:02:44 +0100

    Actually, deep reload sucks.

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -4,4 +4,4 @@ c.TerminalInteractiveShell.prompt_in1 = '$ '
 c.TerminalInteractiveShell.prompt_out = '> '
 c.TerminalIPythonApp.gui = 'gtk'
 c.TerminalIPythonApp.pylab = 'gtk'
-c.TerminalInteractiveShell.deep_reload = True
+#c.TerminalInteractiveShell.deep_reload = True
