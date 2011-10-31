e0f7385957335401a31a1d994ca9a8e60828daab
Antoine Levitt <antoine.levitt@gmail.com>
2011-10-31 14:03:58 +0100

    Remove old python.el code

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -559,16 +559,6 @@ some other pops up with display-buffer), go back to only one window open"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'python)
-(setq
- python-shell-interpreter "ipython"
- python-shell-prompt-regexp "$ "
- python-shell-prompt-output-regexp "> "
- python-shell-completion-setup-code
- "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
- "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
- "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 (defun ipython-run ()
   (interactive)
