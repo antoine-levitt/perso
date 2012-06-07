aa8787d716f9f6603d0d34bc8a2340341106d32a
Antoine Levitt <antoine.levitt@gmail.com>
2012-06-07 15:24:13 +0200

    fix webwatch

--- bin/webwatch.sh
+++ bin/webwatch.sh
@@ -9,7 +9,7 @@ wget -q "$URL" -O "/tmp/$NAME.new"
 
 if [ ! -f "/tmp/$NAME.ref" ]; then
     mv "/tmp/$NAME.new" "/tmp/$NAME.ref"
-elif [ "$(cat $NAME.ref | md5sum)" != "$(cat /tmp/$NAME.new | md5sum)" ]; then
+elif [ "$(cat /tmp/$NAME.ref | md5sum)" != "$(cat /tmp/$NAME.new | md5sum)" ]; then
     gnome-osd-client "New changes"
     mv "/tmp/$NAME.new" "/tmp/$NAME.ref"
 else
