707d4b256683d1c048e128140ad279f9371698f5
Antoine Levitt <antoine.levitt@gmail.com>
2012-08-19 20:23:55 +0200

    toggle timestamps in erc

--- .emacs.d/erc.el
+++ .emacs.d/erc.el
@@ -425,7 +425,8 @@ erc-modified-channels-alist, filtered by erc-tray-ignored-channels."
   (local-set-key (kbd "C-c C-q") 'erc-query-prompt)
   (local-set-key (kbd "C-c C-n") 'erc-names-prompt)
   (local-set-key (kbd "C-c C-w") 'erc-whois-prompt)
-  (local-set-key (kbd "C-c C-l") 'erc-browse-log))
+  (local-set-key (kbd "C-c C-l") 'erc-browse-log)
+  (local-set-key (kbd "C-c C-s") 'erc-toggle-timestamps))
 (add-hook 'erc-mode-hook 'erc-setup-my-commands)
 
 ;; I don't know why, something messes up with erc-bol, so I'm redefining it
