83e910fafd7295d731dc0d9ac16704a357a46914
Antoine Levitt <antoine.levitt@gmail.com>
2016-02-09 20:59:08 +0100

    remove zenburn things in customize

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -1,27 +1,3 @@
-(require 'zenburn)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- `(diredp-date-time ((t (:foreground ,zenburn-green+1))))
- `(diredp-dir-heading ((t (:foreground ,zenburn-blue-1))))
- `(diredp-dir-priv ((t (:foreground ,zenburn-blue))))
- `(diredp-exec-priv ((t nil)))
- `(diredp-file-name ((t (:foreground ,zenburn-fg))))
- `(diredp-file-suffix ((t (:foreground ,zenburn-fg))))
- `(diredp-flag-mark ((t (:foreground ,zenburn-red))))
- `(diredp-flag-mark-line ((t (:foreground ,zenburn-red))))
- `(diredp-inode+size ((t (:foreground ,zenburn-blue-1))))
- `(diredp-link-priv ((t nil)))
- `(diredp-no-priv ((t nil)))
- `(diredp-other-priv ((t nil)))
- `(diredp-rare-priv ((t nil)))
- `(diredp-read-priv ((t nil)))
- `(diredp-symlink ((t (:foreground ,zenburn-blue-3))))
- `(diredp-write-priv ((t nil)))
- '(link ((((class color) (min-colors 88) (background light)) (:underline t)) (((class color) (background light)) (:underline t)) (((class color) (min-colors 88) (background dark)) (:underline t)) (((class color) (background dark)) (:underline t)) (t (:inherit underline)))))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
