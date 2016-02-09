7e9f9ca2a4803a6041144d739c4b725205579cf9
Antoine Levitt <antoine.levitt@gmail.com>
2016-02-09 21:12:25 +0100

    custom safe themes (?)

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("82e83c9ed132341b83ca8b856ffc6ccadf48b8bf9a16e38ba084d67c85ec2d69" default)))
  '(inhibit-startup-echo-area-message (user-login-name))
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
