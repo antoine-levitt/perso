711950ebd05997612c5d4a61d15f1fa38ff6846e
Antoine Levitt <antoine.levitt@gmail.com>
2016-02-09 20:53:16 +0100

    delete old stuff

--- .ipython/profile_default/ipython_config.py
+++
@@ -1,7 +0,0 @@
-# Configuration file for ipython.
-c = get_config()
-c.TerminalInteractiveShell.prompt_in1 = '$ '
-c.TerminalInteractiveShell.prompt_out = '> '
-c.TerminalIPythonApp.gui = 'gtk'
-c.TerminalIPythonApp.pylab = 'gtk'
-#c.TerminalInteractiveShell.deep_reload = True
--- .matlab/userfuns/export.m
+++
@@ -1,8 +0,0 @@
-%%
-% Exports a figure in fig, eps and pdf formats.
-function export (name)
-    setup_export;
-    
-    saveas(gcf, name);
-    saveas(gcf, name, 'epsc2');
-    system(['epstopdf ' name '.eps']);
