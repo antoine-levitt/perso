49e3e757fd1017fb8db2162e8bf364f81c6d3e8d
Antoine Levitt <antoine.levitt@gmail.com>
2016-11-21 18:34:57 +0100

    safe theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,6 +9,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(horizontal-scroll-bar-mode nil)
  '(inhibit-startup-echo-area-message (user-login-name))
  '(inhibit-startup-screen t)
