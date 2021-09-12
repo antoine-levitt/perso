87a40f449e11b570d7bd5f5c47850fcdb90d0ccc
Antoine Levitt <antoine.levitt@gmail.com>
2021-09-12 14:27:09 +0200

    faces

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,6 +25,13 @@
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(gnus-button ((t (:inherit nil)))))
+
 ;; Packages
 (require 'package)
 (add-to-list 'package-archives
