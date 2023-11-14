ad5d109847521e48d160bdf2ea59b7b1685db009
Antoine Levitt <antoine.levitt@gmail.com>
2023-11-14 10:20:53 +0100

    up

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1684,6 +1684,7 @@ ALL-MAILS are the all the unread emails"
 (global-set-key (kbd "s-r") (lambda () (interactive) (when (> AL-mail-count 0) (mu4e-headers-search "flag:unread AND (maildir:/INBOX OR maildir:/InriaBox/INBOX OR maildir:/OrsayBox/INBOX)" nil nil t nil nil)))) ; last t: open first message
 (global-set-key (kbd "s-g") (lambda () (interactive) (mu4e-headers-search "flag:unread" nil nil t nil t))) ; last t: open first message
 (global-set-key (kbd "s-g") (lambda () (interactive) (mu4e-headers-search "flag:unread" nil nil t nil nil))) ; last t: open first message
+(global-set-key (kbd "s-v") (lambda () (interactive) (mu4e-headers-search "flag:unread AND NOT (maildir:/INBOX OR maildir:/InriaBox/INBOX OR maildir:/OrsayBox/INBOX)" nil nil t nil nil)))
 (defun mu4e--main-view (&optional refresh) nil) ;; too extreme? Bof.
 (global-set-key (kbd "C-x m") 'mu4e-compose-new)
 (define-key mu4e-compose-mode-map (kbd "M-q") nil)
